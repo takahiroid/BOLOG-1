7837f55a03859efd62530718ea240558cc8bd215
松本尚裕 <matsumoto@hitsujistudio.com>
2018-07-24 13:19:34 +0900

    START

---
+++ BOLOG#1/GameListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameListViewController.swift
+//  BOLOG#1
+//
+//  Created by 松本尚裕 on 2018/07/24.
+//  Copyright © 2018年 Takahiro Matsumoto. All rights reserved.
+//
+
+import UIKit
+
+class GameListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
